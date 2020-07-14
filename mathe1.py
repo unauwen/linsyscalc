32e2c382026e5b58d02bbfa776800fe67ec654ab
unauwen <49079500+unauwen@users.noreply.github.com>
2020-07-14 15:14:43 +0200

    Add files via upload

---
+++ mathe1.py
@@ -0,0 +1,13 @@
+matrix = [[2, -4, 0, 0], [1, -3, 0, 1], [1, 0, -1, 3], [3, -4, 3, -1]]
+
+answers = [[-10, -8], [-4, -2], [4, 9], [-11, -15]]
+
+for item in matrix:
+    if len(item) != len(matrix) or len(answers) != len(matrix):
+        print("Error in Data")
+
+for i in range(len(matrix)):
+    j = float(matrix[i][i])
+    for item in matrix[i]
+    item = item / j
+    
